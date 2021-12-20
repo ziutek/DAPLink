75404d2d737162e898345d863027287dea1f5a53
Michał Derkacz <michal@Lnet.pl>
2021-12-20 15:42:44 +0100

    support STM32F103 based ST-LINK/V2 (clones) with ST bootloader

--- source/hic_hal/device.h
+++ source/hic_hal/device.h
@@ -40,6 +40,8 @@
 #include "sam3u2c.h"
 #elif defined (INTERFACE_STM32F103XB)
 #include "stm32f1xx.h"
+#elif defined (INTERFACE_STLINKCLONE)
+#include "stm32f1xx.h"
 #elif defined (INTERFACE_M48SSIDAE)
 #include "M480.h"
 #elif defined (INTERFACE_NRF52820)
